fc669bb8e989c6a44c2d70f5d03c72afeee7a278
Constantine <foofightermail@gmail.com>
2026-02-02 00:11:57 +0300

    "added build.gradle.kts, props and gradlew to App trying to fix CI"

---
+++ app/build.gradle.kts
@@ -0,0 +1,50 @@
+import org.gradle.kotlin.dsl.property
+
+plugins {
+    id("java")
+    id("se.patrikerdes.use-latest-versions") version "0.2.19"
+    id("com.github.ben-manes.versions") version "0.41.0"
+    application
+    checkstyle
+
+    id("org.sonarqube") version "7.2.2.6593"
+}
+    sonar {
+        properties {
+            property("sonar.projectKey", "Constantine72_qa-auto-engineer-java-project-61")
+            property("sonar.organization", "constantine72")
+        }
+
+    }
+
+    application {
+        mainClass.set("hexlet.code.App")
+    }
+
+    group = "hexlet.code"
+    version = "1.0-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        testImplementation(platform("org.junit:junit-bom:5.10.0"))
+        testImplementation("org.junit.jupiter:junit-jupiter")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    }
+
+    tasks.test {
+        useJUnitPlatform()
+    }
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
+    }
+
+    tasks.getByName("run", JavaExec::class) {
+        standardInput = System.`in`
+
+    }

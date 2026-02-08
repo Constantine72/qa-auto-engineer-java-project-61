c8e2ba7b249a4cfa93e45ae62e1939e12ae65d90
Constantine <foofightermail@gmail.com>
2026-02-08 19:10:31 +0300

    "added separate method for calculation"

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -19,35 +19,46 @@ public final class Calc {
 
             int randomNumber1 = random.nextInt(randomDigit) + 1;
             int randomNumber2 = random.nextInt(randomDigit) + 1;
+
             char[] operations = {'+', '-', '*'};
+
             char operation = operations[random.nextInt(operations.length)];
 
-            int result = 0;
-
-
-            switch (operation) {
-                case '+':
-                    result = randomNumber1 + randomNumber2;
-                    break;
-                case '-':
-                    result = randomNumber1 - randomNumber2;
-                    break;
-                case '*':
-                    result = randomNumber1 * randomNumber2;
-                    break;
-                default:
-                    System.out.println("Unknown operation" + operation);
-            }
+            int result = calculate(randomNumber1, randomNumber2, operation);
 
             String question = randomNumber1 + " " + operation + " " + randomNumber2;
             String correctAnswer = String.valueOf(result);
 
             rounds[i][0] = question;
             rounds[i][1] = correctAnswer;
+
         }
+
         Engine.run(rules, rounds);
     }
+
+    private static int calculate(int randomNumber1, int randomNumber2, char operation) {
+        int result = 0;
+
+        switch (operation) {
+
+            case '+':
+                result = randomNumber1 + randomNumber2;
+                break;
+            case '-':
+                result = randomNumber1 - randomNumber2;
+                break;
+            case '*':
+                result = randomNumber1 * randomNumber2;
+                break;
+
+            default:
+                System.out.println("Unknown operation" + operation);
+        }
+        return result;
+    }
 }
+
 
 
 

7ef1f16937199978e6adab008268e762ca5c45d9
Constantine <foofightermail@gmail.com>
2026-03-07 01:19:02 +0300

    "updated build.gradle.kts with junit dependencies"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.kotlin.dsl.property
-
 plugins {
     //id("se.patrikerdes.use-latest-versions") version "0.2.19"
     //id("com.github.ben-manes.versions") version "0.41.0"
@@ -7,31 +5,41 @@ plugins {
     checkstyle
     id("org.sonarqube") version "7.2.2.6593"
 }
-    sonar {
-        properties {
-            property("sonar.projectKey", "Constantine72_qa-auto-engineer-java-project-61")
-            property("sonar.organization", "constantine72")
-        }
-
+sonar {
+    properties {
+        property("sonar.projectKey", "Constantine72_qa-auto-engineer-java-project-61")
+        property("sonar.organization", "constantine72")
     }
 
-    application {
-        mainClass.set("hexlet.code.App")
-    }
+}
 
-    group = "hexlet.code"
-    version = "1.0-SNAPSHOT"
+application {
+    mainClass.set("hexlet.code.App")
+}
 
-    repositories {
-        mavenCentral()
-    }
-    //java {
-     //   toolchain {
-     //       languageVersion.set(JavaLanguageVersion.of(17))
-       // }
-    //}
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
 
-    //tasks.getByName("run", JavaExec::class) {
-        //standardInput = System.`in`
+repositories {
+    mavenCentral()
+}
 
-   // }
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+//java {
+//   toolchain {
+//       languageVersion.set(JavaLanguageVersion.of(17))
+// }
+//}
+
+//tasks.getByName("run", JavaExec::class) {
+//standardInput = System.`in`
+
+// }
